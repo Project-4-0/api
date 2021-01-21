24f5a42a9a1f2bbb2678aac4974edc468d39a2e1
zyraix <r0660626@student.thomasmore.be>
2021-01-21 14:50:38 +0100

    API met MVC structuur

---
+++ app/routes/boxRoute.js
@@ -0,0 +1,17 @@
+//express framework
+const express = require("express");
+const router = express.Router();
+
+//Controller import
+const boxController = require('../controllers/boxController');
+
+//Functions
+router.get("/", boxController.box_indexs);
+router.get("/:id", boxController.box_index);
+router.post("/", boxController.box_create);
+router.put("/:id", boxController.box_update);
+router.delete("/:id", boxController.box_delete);
+
+//Export route
+module.exports = router;
+  
