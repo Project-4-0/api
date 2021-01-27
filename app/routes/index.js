21ce205dfbd4797580dafa35b2cbb01cf966b973
YouriVLTM <info@yourivanlaer.be>
2021-01-27 10:16:23 +0100

    fixed measuremnt

--- app/routes/index.js
+++ app/routes/index.js
@@ -32,8 +32,8 @@ router.delete("/boxes/:id", boxController.delete);
 router.get("/measurements", measurementController.list);
 router.get("/measurements/:id", measurementController.getById);
 router.post("/measurements", measurementController.add);
-router.put("/measurements", measurementController.update);
-router.delete("measurements/:id", measurementController.delete);
+// router.put("/measurements", measurementController.update);
+// router.delete("measurements/:id", measurementController.delete);
 
 /* Locations Router */
 router.get("/locations", locationController.list);
@@ -43,11 +43,11 @@ router.put("/locations", locationController.update);
 router.delete("/locations/:id", locationController.delete);
 
 /* BoxUser Router */
-router.get("/boxUsers", boxUserController.list);
-router.get("/boxUsers/:id", boxUserController.getById);
-router.post("/boxUsers", boxUserController.add);
-router.put("/boxUsers", boxUserController.update);
-router.delete("/boxUsers/:id", boxUserController.delete);
+// router.get("/boxUsers", boxUserController.list);
+// router.get("/boxUsers/:id", boxUserController.getById);
+// router.post("/boxUsers", boxUserController.add);
+// router.put("/boxUsers", boxUserController.update);
+// router.delete("/boxUsers/:id", boxUserController.delete);
 
 /* Monitoring Router */
 router.get("/monitoring", monitoringController.list);
