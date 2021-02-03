decb39634ca309def84512b35b60f4a8132b31cc
YouriVLTM <info@yourivanlaer.be>
2021-02-03 19:42:12 +0100

    Update weather.js

--- app/controllers/weather.js
+++ app/controllers/weather.js
@@ -4,33 +4,50 @@ const Sensor = require("../models").Sensor;
 const Box = require("../models").Box;
 const User = require("../models").User;
 const SensorType = require("../models").SensorType;
-
+const BoxUser = require("../models").BoxUser;
 const Location = require("../models").Location;
 
+const { Op } = require("sequelize");
 const axios = require("axios");
 
 //Models
 module.exports = {
   async getWeather(req, res) {
     try {
-      // get box with lat long
+      //box
+      var box = await Box.findByPk(req.params.id);
+      if (!box) {
+        return res.status(404).send({
+          message: "box Not Found",
+        });
+      }
 
-      let location = await Location.findAll({
-        where: { EndDate: null },
-        order: [["LocationID", "DESC"]],
-        include: [
-          {
-            all: true,
-            paranoid: true,
-            where: { BoxID: req.params.id, EndDate: null },
+      var boxUser = await BoxUser.findOne({
+        where: {
+          EndDate: null,
+          StartDate: {
+            [Op.not]: null, // Like: sellDate IS NOT NULL
           },
-        ],
+          BoxID: box.BoxID,
+        },
+        order: [["StartDate", "DESC"]],
       });
+
+      let location = await Location.findOne({
+        where: { BoxUserID: boxUser.BoxUserID },
+        order: [["StartDate", "DESC"]],
+      });
+
+      if (!location) {
+        return res.status(404).send({
+          message: "Location Not Found",
+        });
+      }
 
       // call weather api
       let api_key = process.env.OPENWEATHER_APIKEY;
-      let lat = "51.14612394592471";
-      let long = "5.002733117193132";
+      let lat = location.Latitude;
+      let long = location.Longitude;
 
       let url =
         "https://api.openweathermap.org/data/2.5/weather?lat=" +
@@ -41,12 +58,9 @@ module.exports = {
         api_key +
         "&units=metric";
 
-      console.log(api_key);
-      console.log(url);
-
       var responseServer = await axios.get(url);
 
-      return res.status(200).send({ result: responseServer.data, location });
+      return res.status(200).send({ result: responseServer.data });
     } catch (error) {
       return res.status(400).send(error);
     }
